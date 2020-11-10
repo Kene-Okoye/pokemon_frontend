d1f5e0e6386393875ee16316c6151d36b58f3485
Teresa Tetens <teresa.tetens@gmail.com>
2020-11-10 17:51:01 +0100

    add frontend route

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {createBrowserHistory} from 'history';
+
+const history = createBrowserHistory();
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter history = {history}>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
